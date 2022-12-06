6d18a20992e071a33aa9b98697eb65e87cfb0b5a
Adrian Black <adrian@slowarc.com>
2022-12-05 17:25:31 -0800

    minimal expiration logic

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,11 @@ use std::hash::{Hash, Hasher};
 
 type MapEntry<T> = Vec<(String, T, Option<(Instant, Duration)>)>;
 
+/// A simple hashmap with a flexible map size.
+///
+/// [N] controls the size of the internal map structure, and is a compile-time
+/// constant. A larger [N] will allow faster lookups for large sets, at the expense
+/// of higher initial memory usage.
 pub struct MiniMap<const N: usize, T> {
     pub(crate) map: [MapEntry<T>; N]
 }
@@ -18,19 +23,26 @@ impl<const N: usize, T> MiniMap<N, T> {
         (hasher.finish() % N as u64) as usize
     }
 
-    pub fn insert(&mut self, key: &str, value: T) {
+    /// Insert or replace an item at the given key. An optional [ttl]
+    /// may be set to control expiration of an item.
+    pub fn insert(&mut self, key: &str, value: T, ttl: Option<Duration>) {
         // hash the key and find the corresponding slot in our map
         let idx = Self::hash(key);
         let slot: &mut MapEntry<T> = &mut self.map[idx];
+
+        let expiration = ttl.map(|d| (Instant::now(), d));
 
         // find and update the value, or insert a new entry
         let item = slot.iter_mut().find(|i| i.0 == key);
         match item {
             Some(i) => i.1 = value,
-            None => slot.push((key.to_string(), value, None))
+            None => slot.push((key.to_string(), value, expiration))
         }
     }
 
+    /// Get the item at the given key, if it exists and is not expired.
+    /// If an item exists and IS expired, None will be returned, but the
+    /// item will not be permanently lost until [expire()] is called.
     pub fn get(&self, key: &str) -> Option<(&T, Option<Duration>)> {
         // hash the key and find the corresponding slot in our map
         let idx = Self::hash(key);
@@ -39,11 +51,18 @@ impl<const N: usize, T> MiniMap<N, T> {
         // find and return a ref to the item
         let item = slot.iter().find(|i| i.0 == key);
         match item {
-            Some(i) => Some((&i.1, None)),
-            None => None
+            None => None,
+            Some(i) => {
+                if let Some((stamp, duration)) = i.2 {
+                    if stamp.elapsed() > duration { return None; }
+                }
+                Some((&i.1, i.2.map(|d| d.1 - d.0.elapsed())))
+            },
         }
     }
 
+    /// Remove the item at the given key, and return it. Expiration status,
+    /// if any, is ignored.
     pub fn remove(&mut self, key: &str) -> Option<T> {
         // hash the key and find the corresponding slot in our map
         let idx = Self::hash(key);
@@ -57,12 +76,24 @@ impl<const N: usize, T> MiniMap<N, T> {
         }
     }
 
+    /// Checks the expiration status of all keys, and permanently removes any expired items. A count
+    /// of expired items is returned.
+    pub fn expire(&mut self) -> usize {
+        let expired: Vec<String> = self.map.iter_mut().flatten().filter(|i| i.2.is_some()).filter_map(|i| {
+            let stamp = i.2.unwrap();
+            if stamp.0.elapsed() > stamp.1 { Some(i.0.clone()) } else { None }
+        }).collect();
+
+        for key in &expired {
+            self.remove(key);
+        }
+        expired.len()
+    }
+
+    /// Returns the total number of keys in the map, ignoring expiration status.
     pub fn len(&self) -> usize {
         self.map.iter().map(|i| i.len()).sum()
     }
-}
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
 }
 
 #[cfg(test)]
@@ -72,7 +103,7 @@ mod tests {
     #[test]
     fn can_insert_new() {
         let mut map: MiniMap<128, String> = MiniMap::new();
-        map.insert("foo", "bar".to_string());
+        map.insert("foo", "bar".to_string(), None);
 
         assert_eq!("bar", map.get("foo").unwrap().0);
     }
@@ -81,9 +112,9 @@ mod tests {
     fn can_insert_with_collision() {
         // very small hash size to maximize possibility of collision
         let mut map: MiniMap<2, String> = MiniMap::new();
-        map.insert("foo", "1".to_string());
-        map.insert("bar", "2".to_string());
-        map.insert("baz", "3".to_string());
+        map.insert("foo", "1".to_string(), None);
+        map.insert("bar", "2".to_string(), None);
+        map.insert("baz", "3".to_string(), None);
 
         // first three keys above happen to have odd hashes, so they all end up in the second bucket
         assert_eq!(3, map.map[1].len());
@@ -92,8 +123,8 @@ mod tests {
     #[test]
     fn can_get() {
         let mut map: MiniMap<128, String> = MiniMap::new();
-        map.insert("foo", "bar".to_string());
-        map.insert("baz", "bat".to_string());
+        map.insert("foo", "bar".to_string(), None);
+        map.insert("baz", "bat".to_string(), None);
 
         assert_eq!("bat", map.get("baz").unwrap().0);
     }
@@ -101,11 +132,27 @@ mod tests {
     #[test]
     fn can_remove() {
         let mut map: MiniMap<128, String> = MiniMap::new();
-        map.insert("foo", "bar".to_string());
-        map.insert("baz", "bat".to_string());
+        map.insert("foo", "bar".to_string(), None);
+        map.insert("baz", "bat".to_string(), None);
 
         assert_eq!(Some("bat".to_string()), map.remove("baz"));
         assert_eq!(None, map.remove("xyz"));
         assert_eq!(None, map.get("baz"));
+    }
+
+    #[test]
+    fn can_expire() {
+        let mut map: MiniMap<128, String> = MiniMap::new();
+        map.insert("foo", "bar".to_string(), Some(Duration::from_millis(1)));
+
+        assert_eq!("bar", map.get("foo").unwrap().0);
+        assert_eq!(1, map.len());
+
+        std::thread::sleep(Duration::from_millis(2));
+        assert_eq!(None, map.get("foo"));
+        assert_eq!(1, map.len());
+
+        assert_eq!(1, map.expire());
+        assert_eq!(0, map.len());
     }
 }
